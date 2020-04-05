3b13b30a4d8bc8e5a5a58c11893ea366009beb01
Nikita Zinovev <nrz2000@mail.ru>
2020-04-05 22:55:38 +0300

    Add getProducts, createrProduct, setMessages, updateSkills methods in model

--- 03-express-app/server/model/index.js
+++ 03-express-app/server/model/index.js
@@ -4,7 +4,7 @@ const adapter = new FileSync('./server/model/db.json');
 const db = low(adapter);
 
 module.exports = {
-  getAdminInfo: (resolve, reject) => {
+  getAdminInfo: () => {
     try {
       const result = db.get('admin').value();
       return result;
@@ -16,6 +16,45 @@ module.exports = {
     try {
       const result = db.get('skills').value();
       return result;
+    } catch (e) {
+      console.error(e);
+    }
+  },
+  getProducts: () => {
+    try {
+      const result = db.get('products').value();
+      return result;
+    } catch (e) {
+      console.error(e);
+    }
+  },
+  updateSkills: (skills) => {
+    try {
+      Object.entries(skills).forEach(([id, number]) => (
+        db.get('skills')
+          .find({ id })
+          .assign({ number })
+          .write()
+      ));
+    } catch (e) {
+
+    }
+  },
+  setMessages: (message) => {
+    try {
+      db.get('messages')
+        .push(message)
+        .write();
+    } catch (e) {
+      console.error(e);
+    }
+  },
+  createProduct: (product) => {
+    console.log(product);
+    try {
+      db.get('products')
+        .push(product)
+        .write();
     } catch (e) {
       console.error(e);
     }
