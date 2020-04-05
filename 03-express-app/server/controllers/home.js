cbe58e74d43e8c1bc07ba8c0e5ab4e991429360f
Nikita Zinovev <nrz2000@mail.ru>
2020-04-05 22:52:43 +0300

    Complete logic home page

--- 03-express-app/server/controllers/home.js
+++ 03-express-app/server/controllers/home.js
@@ -1,19 +1,21 @@
 const db = require('../model');
-const { getSkills } = db;
+const { getSkills, setMessages, getProducts } = db;
 
 module.exports = {
   getHomePage: (req, res) => {
     const skills = getSkills();
+    const products = getProducts();
     res.render('pages/home', {
-      title: 'Home page',
+      title: 'Home',
       skills,
-      products: [],
+      products,
       msgemail: req.flash('msgemail')[0]
     });
   },
   sendMessage: (req, res) => {
     try {
       console.log(req.body);
+      setMessages(req.body);
       req.flash('msgemail', 'Форма обработана');
     } catch (e) {
       req.flash('msgemail', e.message);
