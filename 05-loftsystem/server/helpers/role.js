9916bf890d61636fd13a587f04c4e935887db400
Nikita Zinovev <nrz2000@mail.ru>
2020-04-18 01:11:54 +0300

    Add initial sockets connection via sockets.io

--- 05-loftsystem/server/helpers/role.js
+++ 05-loftsystem/server/helpers/role.js
@@ -2,7 +2,7 @@ module.exports = {
   user: {
     chat: { C: false, R: true, U: false, D: false },
     news: { C: false, R: true, U: false, D: false },
-    settings: { C: false, R: true, U: true, D: false },
+    settings: { C: false, R: false, U: false, D: false },
   },
   admin: {
     chat: { C: true, R: true, U: true, D: true },
--- 05-loftsystem/server/index.js
+++ 05-loftsystem/server/index.js
@@ -2,7 +2,46 @@ const express = require('express');
 const path = require('path');
 require('dotenv').config();
 const app = express();
+const server = require('http').createServer(app);
+const io = require('socket.io')(server);
 const db = require('./config/db');
+
+const users = {};
+
+io.on('connection', (socket) => {
+  socket.on('users:connect', (userId, username) => {
+    users[socket.id] = {
+      userId: userId,
+      socketId: socket.id,
+      username: username,
+      activeRoom: null,
+    };
+    socket.emit('users:list', Object.entries(users).map(([socketId, socketIdValue]) => socketIdValue));
+    socket.broadcast.emit('users:add', users[socket.id]);
+  });
+
+  socket.on('message:add', ({ senderId, recipientId, roomId, text }) => {
+    // socket.json.emit('message:add', {
+    //   text,
+    //   senderId,
+    //   recipientId,
+    // });
+  });
+
+  socket.on('message:history', ({ recipientId, userId }) => {
+    // socket.json.emit('message:history', {
+    //   userId,
+    //   recipientId,
+    // });
+  });
+
+  socket.on('disconnect', (data) => {
+    const room = users[socket.id].activeRoom;
+    socket.leave(room);
+    socket.emit('users:leave', socket.id);
+    delete users[socket.id];
+  });
+});
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
@@ -14,6 +53,6 @@ app.get('*', (req, res) => {
   res.sendFile(path.join(__dirname, '../build/index.html'));
 });
 
-app.listen('3000', () => {
+server.listen('3000', () => {
   console.log('Server listening on http://localhost:3000');
 });
